84735a9a7805bac609ffea1f79ee0ce8a4056dbd
mswanbor <mswanbor@cisco.com>
2017-05-22 18:31:20 +1000

    Improved setup.py details

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup (
     name='pygments_yang_lexer',
+    version = "0.0.2",
+    author = "Carl Moberg",
+    description = ("A pygments lexer for Yang."),
+    long_description=read('README.md'),
     packages=find_packages(),
     entry_points =
     """
